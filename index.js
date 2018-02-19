5fa28c6a3624f83846b825b8ecf8a95b37499b34
Doris Ngo Momha <dorismha@icloud.com>
2018-02-19 21:32:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
 function happyHolidayTo(holiday, name) {
-  return "Happy Independance Day, you!"
+  return "Happy Independence Day, you!"
 }
