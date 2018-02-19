31ddf34e67ec3ad9528d0bc723df75a0ddab39dd
Doris Ngo Momha <dorismha@icloud.com>
2018-02-19 21:39:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidayTo(holiday, name) {
   return "Happy Independence Day, you!"
 }
 function holidayCountdown(holiday, days) {
-  return 'It\'s 2O days until Mother\'s Day!"
+  return "It's 20 days until Mother's Day!"
 }
